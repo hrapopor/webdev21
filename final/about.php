c7e26b2333a036ed46d175dcd315cfe371504df9
hrapopor <65172864+hrapopor@users.noreply.github.com>
2021-05-05 08:23:51 -0400

    Create about.php

---
+++ final/about.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+include_once 'header.php';
+require_once 'login.php';
+?>
+<p style="width:70%"> Over this past year, I have spent much of my time looking at my bookshelf while working from home. My library is largely comprised of non-fiction and academic books relating to book history, art history, museum studies, and religious history. I rarely, if ever, purchase books from large booksellers like Amazon or Barnes and Noble. Instead my collection is sourced from used bookstores (I always try to go to at least one in every city I visit), Little Free Libraries and gifts from friends and family.</p>
+<p style="width: 70%">Created for INFO-638 Web Development, Spring 2021</p>
+<?php
+include_once 'footer.php';
